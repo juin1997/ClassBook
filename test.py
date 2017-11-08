e4e0c7617ca421c1498f3e85448cd6dcb2fef93a
juin1997 <gggg704@live.com>
2017-11-08 21:41:10 +0800

    修改flask服务配置文件
    1、将接受文件方式改为接受json
    2、将excel返回浏览器下载
    3、修改配置文件让其可以再gunicorn+ngixn环境下运行

--- test.py
+++ test.py
@@ -1,47 +1,61 @@
 #coding:utf-8
-import jsonify
 import xlwt
-from flask import Flask, request
+from flask import Flask, request, jsonify, render_template,send_file,make_response
 from flask_script import Manager 
 import sqlite3
 import json
-app = Flask(__name__)
+import os
 
-@app.route('/export')                                                                    # 将数据库信息转换为excel
-def export():                                                                            
-    conn = sqlite3.connect('test.db')                                                    # 连接数据库
-    cursor = conn.cursor()
-    count = cursor.execute("select * from student")                                      # 执行sql语句
+def create_app():
+  app = Flask(__name__)
+  app.config['JSON_AS_ASCII'] = False
+  return app
 
-    results = cursor.fetchall()                                                          # 搜取所有结果
-    table_name = 'student'
-    fields = cursor.description                                                          # 获取sqlite3里面的数据字段名称
-    workbook = xlwt.Workbook()
-    sheet = workbook.add_sheet('table_'+table_name,cell_overwrite_ok=True)
+application = create_app()
 
-    for field in range(0,len(fields)):                                                   # 写上字段信息
-        sheet.write(0,field,fields[field][0])
-
-    row = 1 
-    col = 0
-    for row in range(1,len(results)+1):                                                  # 获取并写入数据段信息
-        for col in range(0,len(fields)):
-            sheet.write(row,col,'%s'%results[row-1][col])
-
-    workbook.save('demo.xls')
-
-    cursor.close()                                                                       # 关闭数据库连接
-    conn.commit()
-    conn.close()
-    return jsonify({'finished': True})
-
-def tojson(listdata):                                                                    # 返回json格式数据
+def tojson(listdata):                                                                            # 返回json格式数据
     senddata = {}
-    senddata['data'] = listdata
-    print(json.dumps(senddata, ensure_ascii=False))
-    return  json.dumps(senddata, ensure_ascii=False)
+    senddata = listdata
+    return  jsonify(senddata)
 
-@app.route('/show')                                                                      # 显示所有学生信息
+@application.route('/student')
+def index():
+    return render_template('student.html')
+
+@application.route('/export')                                                                    # 将数据库信息转换为excel
+def export():                                 
+    try:             
+        os.remove(os.path.join(app.static_folder, 'ClassBook.xls'))                              
+        conn = sqlite3.connect('test.db')                                                    # 连接数据库
+        cursor = conn.cursor()
+        count = cursor.execute("select* from student")                                      # 执行sql语句
+
+        results = cursor.fetchall()                                                          # 搜取所有结果
+        table_name = 'student'
+        fields = cursor.description                                                          # 获取sqlite3里面的数据字段名称
+        workbook = xlwt.Workbook()
+        sheet = workbook.add_sheet('table_'+table_name,cell_overwrite_ok=True)
+
+        for field in range(0,len(fields)):                                                   # 写上字段信息
+            sheet.write(0,field,fields[field][0])
+
+        row = 1 
+        col = 0
+        for row in range(1,len(results)+1):                                                  # 获取并写入数据段信息
+            for col in range(0,len(fields)):
+                sheet.write(row,col,'%s'%results[row-1][col])
+
+        workbook.save('ClassBook.xls')
+        response = make_response(send_file("ClassBook.xls"))
+        response.headers["Content-Disposition"] = "attachment; filename=ClassBook.xls;"
+        cursor.close()                                                                       # 关闭数据库连接
+        conn.commit()
+        conn.close()
+        return response
+    except: 
+        return jsonify({'finished': 'false'})
+
+@application.route('/show', methods = ['POST','GET'])                                            # 显示所有学生信息
 def show_stu():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
@@ -59,95 +73,88 @@ def show_stu():
             one_record['qq'] = row[5]
             one_record['note'] = row[6]
             listdata.append(one_record)    
+        
+        cursor.close()
+        conn.commit()
+        conn.close()
+        return tojson(listdata)
     except:
         print("Error: unable to fecth data")
-    
-    cursor.close()
-    conn.commit()
-    conn.close()
+        return jsonify({'finished': 'false'})
 
-    return tojson(listdata)
-    
-@app.route('/add', methods = ['POST','GET'])                                             # 添加学生信息
+@application.route('/add', methods = ['POST'])                                             # 添加学生信息
 def add_stu():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    if request.method == 'POST':                                                              # 接受前端发送的信息
-        my_name = request.form.get('name', '')
-        my_address = request.form.get('address','')
-        my_phone = request.form.get('phone','')
-        my_wechat = request.form.get('wechat','')
-        my_mail = request.form.get('mail','')
-        my_qq = request.form.get('qq','')
-        my_note = request.form.get('note','')
-    else:
-        my_name = request.args.get('name', '')
-        my_address = request.args.get('address','')
-        my_phone = request.args.get('phone','')
-        my_wechat = request.args.get('wechat','')
-        my_mail = request.args.get('mail','')
-        my_qq = request.args.get('qq','')
-        my_note = request.args.get('note','')
+    data = request.get_data()
+    data = data.decode("utf-8")
+    dict = json.loads(data)
+    values = (dict['name'],dict['address'],dict['phone'],dict['wechat'],
+            dict['mail'],dict['qq'],dict['note'])                                                  # 接受前端发送的信息
+    print(values)
     try:
-        cursor.execute("insert into student(name,address,phone,wechat,mail,qq,note) values('"+
-                       dform['my_name']+"','"+dform['my_address']+"','"+dform['my_phone']+"','"+dform['my_wechat']+
-                       "','"+dform['my_mail']+"','"+dform['my_qq']+"','"+dform['my_note']+"');")
+        cursor.execute('''insert into student(name,address,phone,wechat,mail,qq,note) 
+                       values(?,?,?,?,?,?,?)''',values)
+        cursor.close()
+        conn.commit()
+        conn.close()
+        return jsonify({'finished': 'true'})
     except:
         print("Error: unable to insert data")
+        return jsonify({'finished': 'false'})
 
-    cursor.close()
-    conn.commit()
-    conn.close()
-    return jsonify({'finished': True})
-
-@app.route('/delete', methods = ['POST','GET'])                                          # 删除学生信息                 
+@application.route('/delete', methods = ['POST'])                                          # 删除学生信息                 
 def delete_stu():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    if request.method == 'POST':
-        phone = request.form.get('phone', '')
-    else:
-        phone = request.args.get('phone', '')
+    data = request.get_data()
+    data = data.decode("utf-8")
+    dict = json.loads(data) 
+    phone = (dict['phone'],)
     try:
-        cursor.execute("delete from student where phone = '"+phone+"';")
+        cursor.execute("DELETE FROM student WHERE phone = ?",phone)
+        cursor.close()
+        conn.commit()
+        conn.close()
+        return jsonify({'finished': 'true'})
     except:
         print("Error: unable to delete data")
-    cursor.close()
-    conn.commit()
-    conn.close()
-    return jsonify({'finished': True})
-
-@app.route('/change', methods = ['POST','GET'])                                          # 更改学生信息
+        return jsonify({'finished': 'false'})
+      
+@application.route('/change', methods = ['POST'])                                          # 更改学生信息
 def change_stu():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    if request.method == 'POST':
-        phone = request.form.get('phone', '')
-    else:
-        phone = request.args.get('phone', '')
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    data = request.get_data()
+    data = data.decode("utf-8")
+    dict = json.loads(data)
+    values = (dict['name'],dict['address'],dict['phone'],dict['wechat'],dict['mail'],dict['qq'],dict['note'])          
+    phone = (dict['phone'],)
     try:
-        cursor.execute("delete from student where phone = '"+dform['phone']+"';")
-        cursor.execute("insert into student(name,address,phone,wechat,mail,qq,note) values('"+
-                       dform['my_name']+"','"+dform['my_address']+"','"+dform['my_phone']+"','"+dform['my_wechat']+
-                       "','"+dform['my_mail']+"','"+dform['my_qq']+"','"+dform['my_note']+"');")
+        cursor.execute("delete from student where phone = ?;",phone)
+        cursor.execute('''insert into student(name,address,phone,wechat,mail,qq,note) 
+                       values(?,?,?,?,?,?,?);''',values)
+        cursor.close()
+        conn.commit()
+        conn.close()
+        return jsonify({'finished': 'true'})
     except:
         print("Error: unable to change data")
-    cursor.close()
-    conn.commit()
-    conn.close()
-    return jsonify({'finished': True})
+        return jsonify({'finished': 'false'})
 
-@app.route('/check', methods = ['POST','GET'])                                           # 查看学生信息 
+@application.route('/check', methods = ['POST'])                                           # 查看学生信息 
 def check_stu():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    if request.method == 'POST':
-        phone = request.form.get('phone', '')
-    else:
-        phone = request.args.get('phone', '')
+    listdata = []
+    data = request.get_data()
+    data = data.decode("utf-8")
+    dict = json.loads(data) 
+    phone = (dict['phone'],)
     try:
-        cursor.execute("select* from student where phone = "+dform['my_phone'])
-        listdata = []
+        cursor.execute("select* from student where phone = ?",phone)
         results = cursor.fetchall()
         for row in results:
             one_record = {}
@@ -159,15 +166,14 @@ def check_stu():
             one_record['qq'] = row[5]
             one_record['note'] = row[6]
             listdata.append(one_record)
-            
+
+        cursor.close()
+        conn.commit()
+        conn.close()
+        return tojson(listdata)        
     except:
         print("Error: unable to check data")
-    
-    cursor.close()
-    conn.commit()
-    conn.close()
-
-    return tojson(listdata)
+        return jsonify({'finished': 'false'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run()
